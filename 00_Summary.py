d992da4121c1f0fab825bf0f91f657aa73d98d79
CR-DataTeam <110788223+CR-DataTeam@users.noreply.github.com>
2023-04-17 09:00:49 -0400

    Update 00_Summary.py

--- 00_Summary.py
+++ 00_Summary.py
@@ -412,7 +412,7 @@ with col1:
     st.markdown("For the time being, Randolph & McDowell's Budget & Actual's are grouped together under the Randolph line item.")
 
 with col2:
-    st.markdown("2+10 Forecast has been added.")
+    st.markdown("3+9 Forecast has been added.")
     st.markdown("March 2023 actuals have been updated.")
     link = '[Folder with Forecast Exports](https://usradiology-my.sharepoint.com/:f:/p/joshua_mcdonald/EgPfpgg-y2JCsBxEpAbJv2ABezQly_XsfZHzSKm4zFx9ug?e=F84hhe)'
     st.markdown(link, unsafe_allow_html=True)    
